057b958a6b3263a8bea33ad0f3642cfe84a77f0c
mjmzs <atsignmjmzs@gmail.com>
2018-07-31 15:45:26 +0430

    updated jsx code to advanced js6 to be more clean

--- src/Card.js
+++ src/Card.js
@@ -6,13 +6,13 @@ for (i=0; i < 6 ; i++) {
     roboFace[i] = '"https://robohash.org/roboface' + i + '?180x180"';
 }
 
-const Card = (props) =>{
+const Card = ({id, name, email}) =>{
     return(
         <div className="tc bg-light-green dib br3 pa1 ma2 grow bw shadow-5">
-            <img src={`https://robohash.org/${props.id}?180x180`} alt="RoboFace1" />
+            <img src={`https://robohash.org/${id}?180x180`} alt="RoboFace1" />
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.email}</p>
+                <h3>{name}</h3>
+                <p>{email}</p>
             </div>
         </div>
     );
